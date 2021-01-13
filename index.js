2a414d9cb5e0b0fd6c41f67e3d688b143f6b1bfc
Dexter Fitch <fitch.dexter@gmail.com>
2021-01-13 04:27:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
  fetch('https://anapioficeandfire.com/api/books')
  .then(response => response.json())
- .then(json => return json)
+ .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
